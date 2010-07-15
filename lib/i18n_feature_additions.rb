b92ad4f4f7ca85a74476b5947b2bf2b0c3582301
Dan Nuttall <pixelblend@gmail.com>
2010-07-15 17:36:40 +0100

    [SCR-673] Update perform feature so that features are specified in locales
    Previously, we allowed a feature only if it was for the USA.
    Now, we check the locale file for that country for the feature.feature_name.
    Unless the feature is set and equal to true, the feature cannot be run.
    Default truevalues can be set in the en.yml locale.

--- lib/i18n_feature_additions.rb
+++ lib/i18n_feature_additions.rb
@@ -1,20 +1,9 @@
 module I18n 
   class << self
     def has_feature?(feature)
-      #at the moment, only us countries can use these features
-      begin
-        return I18n.country.code == 'us'
-      rescue
-        return false
-      end
-      
       result = self.t("features.#{feature}")
 
-      if result === true
-        result
-      else
-        raise MissingTranslationData.new('','','')
-      end
+      result === true
     end
   end
 end
